47c757ffe4ae4e013e9c283d5df2f1b597bdbe61
Cristiano Macedo <53766118+cristianomg@users.noreply.github.com>
2019-09-16 20:19:44 -0300

    Update TesteCidade.java

--- Testes/TestesLogradouro/TesteCidade.java
+++ Testes/TestesLogradouro/TesteCidade.java
@@ -11,23 +11,23 @@ public class TesteCidade {
 
 	@Test
 	public void testAddEndereco() {
-		int qtdEndereçoEsperado = 1;
+		int qtdEndereÃ§oEsperado = 10;
 		Cidade cidade = new Cidade(1, "aracaju", "aju", null);
 		Endereco endereco = new Endereco(0, "oceanica", 1077, "atalaia", "49035005", cidade);
 		cidade.addEnderecoLista(endereco);
-		assertEquals(qtdEndereçoEsperado, cidade.getListaEndereco().size());
+		assertEquals(qtdEndereÃ§oEsperado, cidade.getListaEndereco().size());
 		
 	}
 
 	@Test
 	public void testNaoAddEnderecoRepetido() {
-		int qtdEndereçoEsperado = 1;
+		int qtdEndereÃ§oEsperado = 1;
 		Cidade cidade = new Cidade(1, "aracaju", "aju", null);
 		Endereco endereco = new Endereco(0, "oceanica", 1077, "atalaia", "49035005", cidade);
 		Endereco endereco2 = new Endereco(0, "oceanica", 1077, "atalaia", "49035005", cidade);
 		cidade.addEnderecoLista(endereco);
 		cidade.addEnderecoLista(endereco2);
-		assertEquals(qtdEndereçoEsperado, cidade.getListaEndereco().size());
+		assertEquals(qtdEndereÃ§oEsperado, cidade.getListaEndereco().size());
 	}
 
 }
